4378db1f0a4dab9e2dbfb824a870dc9301f9ee78
Andrew Paklin <andrew.paklin@gmail.com>
2018-03-09 23:54:21 +0300

    Begining restyle main sidebar

--- src/app/directives/collapsible-sidebar.directive.ts
+++ src/app/directives/collapsible-sidebar.directive.ts
@@ -6,19 +6,34 @@ import { getOrCreateElementRef } from '@angular/core/src/render3/di';
   selector: '[CollapsibleSidebar]'
 })
 export class CollapsibleSidebarDirective implements DoCheck{
+	private _Collapsed: boolean = false;
+	
 	constructor(
 		private _ApplicationService: ApplicationService,
 		private _Renderer: Renderer2,
 		private _Element: ElementRef
-	) {	}
+	) {	
+		this._Collapsed = this._ApplicationService.SideBarCollapsed;
 
-	ngDoCheck(){
+		this.SwitchCollabsibility( true );
+	}
+
+	private SwitchCollabsibility(OnInit: boolean = false){
+		if(!OnInit && this._Collapsed === this._ApplicationService.SideBarCollapsed){
+			return;
+		}
+
 		var ClassName: string = 'expanded';
-
+		this._Collapsed = this._ApplicationService.SideBarCollapsed;
+		
 		if(this._ApplicationService.SideBarCollapsed){
 			this._Renderer.removeClass( this._Element.nativeElement, ClassName );
 		}else{
 			this._Renderer.addClass( this._Element.nativeElement, ClassName );
 		}
+	}
+
+	ngDoCheck(){
+		this.SwitchCollabsibility();
 	}
 }
